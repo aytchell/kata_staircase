dd751895d065ae6a5b7861092c0eb31584144659
Hannes Lerchl <hannes.lerchl@googlemail.com>
2019-06-25 14:54:50 +0200

    Made a class from the methods
    This way we can reuse the map of dynamic-programming-subresults
    for further calls.

--- main.cpp
+++ main.cpp
@@ -1,40 +1,50 @@
 #include <iostream>
 #include <map>
 
-unsigned int numWay(
-        std::map<unsigned int, unsigned int>& stairs_to_num,
-        unsigned int num_stairs)
+class StairWalker
+{
+    public:
+        unsigned int numWay(unsigned int num_stairs);
+
+    private:
+        unsigned int numWay_internal(unsigned int num_stairs);
+
+        std::map<unsigned int, unsigned int> m_stairs_to_num;
+};
+
+unsigned int StairWalker::numWay_internal(unsigned int num_stairs)
 {
     if (num_stairs == 0) return 1;
     if (num_stairs == 1) return 1;
 
-    const auto& iter = stairs_to_num.find(num_stairs);
-    if (iter != stairs_to_num.end()) return iter->second;
+    const auto& iter = m_stairs_to_num.find(num_stairs);
+    if (iter != m_stairs_to_num.end()) return iter->second;
 
     unsigned int num = 
-        numWay(stairs_to_num, num_stairs - 2) +
-        numWay(stairs_to_num, num_stairs - 1);
-    stairs_to_num.insert(std::make_pair(num_stairs, num));
+        numWay_internal(num_stairs - 2) +
+        numWay_internal(num_stairs - 1);
+    m_stairs_to_num.insert(std::make_pair(num_stairs, num));
 
     return num;
 }
 
-unsigned int numWay(unsigned int num_stairs)
+unsigned int StairWalker::numWay(unsigned int num_stairs)
 {
     if (num_stairs == 0) return 0;
-    std::map<unsigned int, unsigned int> stairs_to_num;
-    return numWay(stairs_to_num, num_stairs);
+    return numWay_internal(num_stairs);
 }
 
 int main()
 {
-    std::cout << "numWay(0) = " << numWay(0) << '\n';
-    std::cout << "numWay(1) = " << numWay(1) << '\n';
-    std::cout << "numWay(2) = " << numWay(2) << '\n';
-    std::cout << "numWay(3) = " << numWay(3) << '\n';
-    std::cout << "numWay(4) = " << numWay(4) << '\n';
-    std::cout << "numWay(5) = " << numWay(5) << '\n';
-    std::cout << "numWay(6) = " << numWay(6) << '\n';
-    std::cout << "numWay(7) = " << numWay(7) << '\n';
-    std::cout << "numWay(8) = " << numWay(8) << '\n';
+    StairWalker walker;
+
+    std::cout << "numWay(0) = " << walker.numWay(0) << '\n';
+    std::cout << "numWay(1) = " << walker.numWay(1) << '\n';
+    std::cout << "numWay(2) = " << walker.numWay(2) << '\n';
+    std::cout << "numWay(3) = " << walker.numWay(3) << '\n';
+    std::cout << "numWay(4) = " << walker.numWay(4) << '\n';
+    std::cout << "numWay(5) = " << walker.numWay(5) << '\n';
+    std::cout << "numWay(6) = " << walker.numWay(6) << '\n';
+    std::cout << "numWay(7) = " << walker.numWay(7) << '\n';
+    std::cout << "numWay(8) = " << walker.numWay(8) << '\n';
 }
