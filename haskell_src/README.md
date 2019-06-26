6d7f38affd72eb874bb78a6414245a4f53097edd
Hannes Lerchl <hannes.lerchl@googlemail.com>
2019-06-26 12:03:16 +0200

    Solved task 2 in Haskell

--- haskell_src/README.md
+++ haskell_src/README.md
@@ -2,5 +2,17 @@
 
 ## numWay n
 
-    *Main> map Staircase.numWay [0..20]
+    *Staircase> map Staircase.numWay [0..20]
     [0,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946]
+
+## numWays n lst
+
+    *Staircase> Staircase.numWays 4 [1,3,5]
+    3
+
+    *Staircase> map (\x -> Staircase.numWays x [1,2]) [1..10]
+    [1,2,3,5,8,13,21,34,55,89]
+
+    *Staircase> map (\x -> Staircase.numWays x [1,3,5]) [1..10]
+    [1,1,2,3,5,8,12,19,30,47]
+
--- haskell_src/Staircase.hs
+++ haskell_src/Staircase.hs
@@ -1,6 +1,11 @@
-module Staircase ( numWay) where
+module Staircase
+( numWay
+, numWays
+) where
 
 numWay :: (Integral n) => n -> n
+numWays :: (Integral n) => n -> [n] -> n
+
 
 numWay_i :: (Integral n) => n -> n
 numWay_i 0 = 1
@@ -9,3 +14,20 @@ numWay_i x = numWay_i (x - 1) + numWay_i (x - 2)
 
 numWay 0 = 0
 numWay x = numWay_i x
+
+
+numWays_i :: (Integral n) => n -> [n] -> n
+numWays_i_ifpossible :: (Integral n) => n -> n -> [n] -> n
+
+numWays_i_ifpossible x y lst
+    | x < y = 0
+    | otherwise = numWays_i (x - y) lst
+
+numWays_i 0 lst = 1
+numWays_i x lst =
+    let nums_per_startstep = map (\y -> numWays_i_ifpossible x y lst) lst
+    in foldl1 (+) nums_per_startstep
+
+numWays 0 lst = 0
+numWays x [] = 0
+numWays x lst = numWays_i x lst
