772d62976aae6b5cf83ea2bcb73f70b38775f690
Hannes Lerchl <hannes.lerchl@googlemail.com>
2019-06-26 11:53:49 +0200

    Made a module from numWay.hs

--- haskell_src/Staircase.hs
+++ haskell_src/Staircase.hs
@@ -1,9 +1,11 @@
+module Staircase ( numWay) where
+
+numWay :: (Integral n) => n -> n
 
 numWay_i :: (Integral n) => n -> n
 numWay_i 0 = 1
 numWay_i 1 = 1
 numWay_i x = numWay_i (x - 1) + numWay_i (x - 2)
 
-numWay :: (Integral n) => n -> n
 numWay 0 = 0
 numWay x = numWay_i x
